8702e00e63d63a518cb86328ac63d302763f9f82
soumyakbhattacharyya <bhattacharyya.soumyak@gmail.com>
2017-05-24 18:36:03 +0530

    refactored rule

--- simple-rule-engine/src/main/java/org/engine/core/Main.java
+++ simple-rule-engine/src/main/java/org/engine/core/Main.java
@@ -1,94 +1,43 @@
 package org.engine.core;
 
-import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.mvel2.MVEL;
-import org.mvel2.ParserContext;
-import org.mvel2.ast.ASTNode;
-import org.mvel2.integration.Interceptor;
-import org.mvel2.integration.VariableResolverFactory;
+import org.engine.core.Rule.CompiledRule;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		final Map output = new HashMap();
 
 		// input.properties
-		Map vars = new HashMap();
-		vars.put(convertHyphenToUnderscore("aws-rds-enabled"), "true");
+		Map input = new HashMap();
+		input.put(Rule.convertHyphenToUnderscore("aws-rds-enabled"), "true");
 
 		// input.product
 
 		Rule rule = Rule.builder().name("AwsRdsEnabled")
-   								  .expression("@Action " + convertHyphenToUnderscore("aws-rds-enabled") + " == true;").namespace("rcu").priority(0)
-								  .outcome(new Action() {
-									 @Override
-									 public void execute() {
-								 		System.out.println("executing some action");
-									 }
-								  }).build();
+								  .expression("@INTERCEPT " + Rule.convertHyphenToUnderscore("aws-rds-enabled") + " == true;").namespace("rcu")
+								  .priority(0)
+								  .action(new Action() {
+									@Override
+									public void execute() {
+										System.out.println("populating outcome");
+										output.put("db-sys-username", "admin");
+										output.put("db-sys-role", "default");
+										output.put("honor-omf", "true");
+									  }
+								    }
+								   )
+								  .build();
 
-		CompiledRule compiledRule = new CompiledRule(rule, vars);
-		MVEL.executeExpression(compiledRule.getCompiled(), vars);
-
+		CompiledRule compiledRule = new CompiledRule(rule, input);
+        compiledRule.eval();
 	}
 
-	public static String convertHyphenToUnderscore(String input) {
-		input = "_" + input.replaceAll("\\-", "_");
-		return input;
-	}
+	
 
-	private static final class CompiledRule {
-		private Rule rule;
-		private Map map;
-		private Serializable compiled;
-
-		private CompiledRule(final Rule rule, final Map map) {
-			this.rule = rule;
-			this.map = map;
-
-			// Create a new ParserContext
-			ParserContext context = new ParserContext();
-
-			Map<String, Interceptor> myInterceptors = new HashMap<String, Interceptor>();
-
-			// Create a simple interceptor.
-			Interceptor myInterceptor = new Interceptor() {
-				public int doBefore(ASTNode node, VariableResolverFactory factory) {
-					System.out.println("BEFORE!");
-					return Interceptor.NORMAL_FLOW;
-				}
-
-				public int doAfter(Object value, ASTNode node, VariableResolverFactory factory) {
-                    if (String.valueOf(value).equalsIgnoreCase("true")){
-                    	rule.getOutcome().execute();
-                    }
-					return Interceptor.NORMAL_FLOW;
-				}
-			};
-
-			// Now add the interceptor to the map.
-			myInterceptors.put("Action", myInterceptor);
-
-			// Add the interceptors map to the parser context.
-			context.setInterceptors(myInterceptors);
-
-			// this.compiled = 
-			compiled = MVEL.compileExpression(rule.getExpression(), context);
-		}
-
-		private Serializable getCompiled() {
-			return compiled;
-		}
-
-		private Rule getRule() {
-			return rule;
-		}
-
-		private boolean eval() {
-			return true; 
-		}
-	}
+	
 
 }
--- simple-rule-engine/src/main/java/org/engine/core/Rule.java
+++ simple-rule-engine/src/main/java/org/engine/core/Rule.java
@@ -1,5 +1,15 @@
 package org.engine.core;
 
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mvel2.MVEL;
+import org.mvel2.ParserContext;
+import org.mvel2.ast.ASTNode;
+import org.mvel2.integration.Interceptor;
+import org.mvel2.integration.VariableResolverFactory;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,10 +23,62 @@ public class Rule {
 
 	private final String name;
 	private final String expression;
-	private final Action outcome;
+	private final Action action;
 	private final int priority;
 	private final String description;
 	private final String namespace;
-	
-	
+
+	public static String convertHyphenToUnderscore(String input) {
+		input = "_" + input.replaceAll("\\-", "_");
+		return input;
+	}
+
+	@Getter
+	public static final class CompiledRule {
+		private Rule rule;
+		private Map input;
+		private Serializable compiled;
+
+		public CompiledRule(final Rule rule, final Map input) {
+			this.rule = rule;
+			this.input = input;
+
+			// Create a new ParserContext
+			ParserContext context = new ParserContext();
+
+			Map<String, Interceptor> interceptors = new HashMap<String, Interceptor>();
+
+			// Create intercepter
+			Interceptor executionInterceptor = new Interceptor() {
+				public int doBefore(ASTNode node, VariableResolverFactory factory) {
+					return Interceptor.NORMAL_FLOW;
+				}
+
+				public int doAfter(Object value, ASTNode node, VariableResolverFactory factory) {
+					if (String.valueOf(value).equalsIgnoreCase("true")) {
+						rule.getAction().execute();
+					}
+					return Interceptor.NORMAL_FLOW;
+				}
+			};
+
+			// Now add the interceptor to the map.
+			interceptors.put("INTERCEPT", executionInterceptor);
+
+			// Add the interceptors map to the parser context.
+			context.setInterceptors(interceptors);
+
+			// this.compiled =
+			compiled = MVEL.compileExpression(rule.getExpression(), context);
+		}
+
+		private Serializable getCompiled() {
+			return compiled;
+		}
+
+		public void eval() {
+			MVEL.executeExpression(getCompiled(), getInput());
+		}
+	}
+
 }
